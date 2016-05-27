209f160fdb07e72ef2e9c74b576419b741afc461
PavlovIS <fierygolemm@gmail.com>
2016-05-27 21:10:47 +0300

    Add files via upload

---
+++ lab1/HW4.py
@@ -0,0 +1,20 @@
+print('Введите строку:')
+s = input()
+#print(len(s))
+n = len(s)
+i = 2
+k = 0
+
+while i < n:
+    if n % i == 0:
+        #print(s[:i])
+        #print(s.count(s[:i]))
+        if n / i == s.count(s[:i]):
+            if s.count(s[:i]) > k:
+                k = s.count(s[:i])
+                t = i
+    i += 1
+print('k=', k, '\n t=', s[:t] )
+
+
+input()
