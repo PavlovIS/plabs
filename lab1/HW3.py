7a48f8e4af58749598c48e6d827f3e7eaf8ed0b1
PavlovIS <fierygolemm@gmail.com>
2016-05-28 12:00:31 +0300

    Update HW3.py

--- lab1/HW3.py
+++ lab1/HW3.py
@@ -1,33 +1,18 @@
-class point:
- def __init__(self,x,y):
-   self.x=x
-   self.y=y
+arr = list()
+for a in input().split(','):
+	arr.append(int(a))
 
-x=input()
-l=list()
-z=list(x)
-u=''
-z.append(',')
-for i in z:
- if i==',':
-  l.append(u)
-  u=''
- else:
-   u=u+i
-print(l)
-a=list()
-i=0
-while i <int(len(l)):
- a.append(point(l[i],l[i+1]))
- i=i+2
-for i in a:
- print(i.x,i.y)
+if len(arr) > 8:
+	print('Значений больше 8!')
+	exit()
+elif len(arr) < 8:
+	print('Значений меньше 8!')
+	exit()
 
-slope=(float(a[0].y)-float(a[1].y))/(float(a[0].x)-float(a[1].x))
-slope2=(float(a[2].y)-float(a[3].y))/(float(a[2].x)-float(a[3].x))
-if slope==slope2:
- print('YES')
+a = (arr[3]-arr[1])/(arr[2]-arr[0])
+b = (arr[7]-arr[5])/(arr[6]-arr[4])
+
+if a == b:
+	print('YES')
 else:
- print('NO')
- 
-print()
+	print('NO')
